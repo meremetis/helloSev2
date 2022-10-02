cf441bffab686d58bdfc20eb75c8656e4f15f594
meremetis <mere.p.w_@hotmail.com>
2022-10-02 19:00:29 +0300

    Hello package

---
+++ src/aueb/sev/Hello/HelloSev.java
@@ -0,0 +1,9 @@
+package aueb.sev.Hello;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class HelloSev {
+    public static void main(String[] args) {
+        System.out.println("hello sev");
+    }
+}
---
+++ src/aueb/sev/Hello/HelloWold.java
@@ -0,0 +1,7 @@
+package aueb.sev.Hello;
+
+public class HelloWold {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}
