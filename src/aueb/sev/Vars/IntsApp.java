295e4624df827fb63d5f70f5765436bf0215046b
meremetis <mere.p.w_@hotmail.com>
2022-10-02 14:02:17 +0300

    Initial Commit

---
+++ src/aueb/sev/Vars/IntsApp.java
@@ -0,0 +1,13 @@
+package aueb.sev.Vars;
+
+public class IntsApp {
+    public static void main(String[] args) {
+        int num = 10;
+        int num2 = 20;
+        int result;
+
+        result = num + num2;
+
+        System.out.println("resulted: "+result);
+    }
+}
