1709c4f26dcc3e56e5c9ee00c205cc713d8cba78
meremetis <mere.p.w_@hotmail.com>
2022-10-02 19:56:27 +0300

    java file change ints v2

--- src/aueb/sev/Vars/IntsApp.java
+++ src/aueb/sev/Vars/IntsApp.java
@@ -2,7 +2,7 @@ package aueb.sev.Vars;
 
 public class IntsApp {
     public static void main(String[] args) {
-        int num = 10;
+        int num = 30;
         int num2 = 20;
         int result;
 
